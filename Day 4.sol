81b49c2c5ea2b503faf21996cc935f8110d63b5e
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-12 13:37:14 +0530

    Update Day 4.sol

--- Day 4.sol
+++ Day 4.sol
@@ -7,7 +7,7 @@ Note - Function will be public.*/
 pragma solidity >=0.5.0 <0.9.0;
 
 contract day4 {
-function evaluate(int a,int b) public view returns(int){
+function evaluate(int a,int b) public pure returns(int){
 int result;
 result=(a+b)-(a-b);
 return result;
