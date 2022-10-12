0ce549bde8bf1049c263c3c406e998322f35cd1d
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-12 12:32:18 +0530

    Create Day 3.sol

---
+++ Day 3.sol
@@ -0,0 +1,22 @@
+/* 1)Create a state variable of uint type (need not to be public).
+
+2)Create a set ( ) and with one argument. And set the value of the created state variable with the value passed as an argument to the set ( ).
+
+3)Then create another function get ( ) which returns the value of the state variable. */
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract FunctionArgument{
+uint private a;
+
+function set(uint b)public{
+a=b;
+}
+
+function get()public view returns(uint){
+return a;
+}
+
+}
