8e4a18497b35c137bfb6d23690575734decc062e
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-12 13:36:44 +0530

    Create Day 5.sol

---
+++ Day 5.sol
@@ -0,0 +1,15 @@
+/*Create a function find(int a). Where a>0.
+Return the remainder when a is divided by 3.
+Note - Function will be public.*/
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day5 {
+
+function find(int a) public view returns (int){
+      int b=a%3;
+      return b;
+    }
+}
